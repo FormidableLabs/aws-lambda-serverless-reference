306420aac54ac3efa66d62e29ea8ce4be685caee
Ryan Roemer <ryan.roemer@formidable.com>
2019-02-22 13:32:51 -0800

    Initial App Release: Base + X-ray serverless apps/handlers. (#7)
    ### Overview
    
    This (again, huge) PR is intended to be the initial release of:
    
    - A new "hello world" reference application that is _just_ an Express app.
    - That does a Terraform bootstrap, and uses our new `terraform-aws-serverless` module for IAM support of the `serverless` Framework application.
    - Has an optional handler / integration of the `xray` submodule of `terraform-aws-serverless`
    
    The companion Terraform module PR to review is: https://github.com/FormidableLabs/terraform-aws-serverless/pull/22
    
    Things I'd suggest to review (if all the files are too beastly):
    
    - Core README: https://github.com/FormidableLabs/aws-lambda-serverless-reference/blob/feature/xray/README.md
    - Main TF integration: https://github.com/FormidableLabs/aws-lambda-serverless-reference/blob/feature/xray/terraform/main.tf
    
    ### Work
    
    Completed:
    
    - Feature/Option: Xray enhancement. Fixes #2
    - Review/remove all source TODOs. Fixes #5
    
    /cc @tptee @jasonwilson @kevinmstephens @mscottx88 @jjasonclark @ianwsperber

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region  = "${var.region}"
+  version = "~> 1.19"
+}
+
+terraform {
+  backend "s3" {
+    key = "terraform.tfstate"
+  }
+}
+
+# Base `serverless` IAM support.
+module "serverless" {
+  source = "FormidableLabs/serverless/aws"
+
+  region       = "${var.region}"
+  service_name = "${var.service_name}"
+  stage        = "${var.stage}"
+
+  # (Default values)
+  # iam_region        = `*`
+  # iam_partition     = `*`
+  # iam_account_id    = `AWS_CALLER account`
+  # tf_service_name   = `tf-SERVICE_NAME`
+  # sls_service_name  = `sls-SERVICE_NAME`
+}
+
+# OPTION(Xray): Add X-ray support to lambda execution roles.
+module "serverless_xray" {
+  source = "FormidableLabs/serverless/aws//modules/xray"
+
+  # Same variables as for `serverless` module.
+  region       = "${var.region}"
+  service_name = "${var.service_name}"
+  stage        = "${var.stage}"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "region" {
+  description = "The deploy target region in AWS"
+  default     = "us-east-1"
+}
+
+variable "stage" {
+  description = "The stage/environment to deploy to. Suggest: `sandbox`, `development`, `staging`, `production`."
+  default     = "development"
+}
+
+variable "service_name" {
+  description = "Name of service / application"
+}
