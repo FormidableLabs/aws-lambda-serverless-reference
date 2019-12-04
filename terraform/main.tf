8c547a71f3c25978e0fd9831f44a1738009dea16
Ryan Roemer <ryan.roemer@formidable.com>
2019-12-03 20:28:02 -0800

    Add RG for stack

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,31 @@ terraform {
 
 # A resource group is an optional, but very nice thing to have, especially
 # when managing resources across CF + TF + SLS.
+#
+# This RG aggregates all of CF + TF + SLS together by `Service` + `Stage`.
+resource "aws_resourcegroups_group" "test" {
+  name = "tf-${var.service_name}-${var.stage}"
+
+  resource_query {
+    query = <<JSON
+{
+  "ResourceTypeFilters": [
+    "AWS::AllSupported"
+  ],
+  "TagFilters": [
+    {
+      "Key": "Service",
+      "Values": ["${var.service_name}"]
+    },
+    {
+      "Key": "Stage",
+      "Values": ["${var.stage}"]
+    }
+  ]
+}
+JSON
+  }
+}
 
 ###############################################################################
 # Base `serverless` IAM support
